b3b088bb490cd972485fedaef8440e3b206e8b2e
Mars <marcsand079@gmail.com>
2020-03-08 20:24:15 -0300

    Create repository && upload main files

---
+++ frontend/JS/index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /*
+    const styleChange = (text, style) => { 
+        switch (style) {
+            case "normal":
+                text.style.fontStyle = style;
+                text.style.fontWeight = "1";
+                break;
+
+            case "bold":
+                text.style.fontWeight = "bold";
+                break;
+
+            default:
+                text.style.fontStyle = style;
+        }
+    }
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            styleChange(text, button.dataset.style);
+            console.log(text.textContent);
+        });
+    });
+    */
+});
+
+ 
