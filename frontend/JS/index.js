73b87109d077eb6f4d0474237b6d64d9a6e59554
Mars <marcsand079@gmail.com>
2020-03-08 22:28:35 -0300

    Create first layouts

--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -1,28 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*
-    const styleChange = (text, style) => { 
-        switch (style) {
-            case "normal":
-                text.style.fontStyle = style;
-                text.style.fontWeight = "1";
-                break;
-
-            case "bold":
-                text.style.fontWeight = "bold";
-                break;
-
-            default:
-                text.style.fontStyle = style;
-        }
-    }
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            styleChange(text, button.dataset.style);
-            console.log(text.textContent);
-        });
-    });
-    */
+  
 });
 
  
